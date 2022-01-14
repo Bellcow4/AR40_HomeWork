db39fd7f1b6b929a04e6ae6bf4d014e0559baa0e
woo3063 <woo3063@naver.com>
2022-01-14 13:55:14 +0900

    박종우 : AR40 HomeWork 커밋

---
+++ CPP/00. HomeWork220112/00. HomeWork220112.cpp
@@ -0,0 +1,48 @@
+﻿// 00. HomeWork220112.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main()
+{
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while ( Index < 10 )
+		{
+			// 0 부터 시작해서
+			Arr[Index];
+			// 0 제외
+			// 2 4 6 8 10 12 14 16 18 
+			// 배열이 가득찰때까지 짝수로만 채워주세요
+
+			Arr[Index] = Index + (Index + 2);
+
+			Index += 1;
+		}
+	}
+
+	{
+		int Arr[10] = { 0, };
+		int Index = 0;
+		while (Index < 10)
+		{
+			// 0 부터 시작해서
+			Arr[Index];
+			// 0 제외
+			// 2 4 8 10 12 14 16 18 
+			// 배열이 가득찰때까지 홀수로만 채워주세요
+
+			Arr[Index] = Index + (Index + 1);
+
+			Index += 1;
+
+		}
+
+		for (int i = 0; i < 10; i += 1)
+		{
+			printf("%d\n", Arr[i]);
+		}
+	}
+}
+
---
+++ CPP/01. HomeWork220113/01. HomeWork220113.cpp
@@ -0,0 +1,35 @@
+﻿// 01. HomeWork220113.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void StringTrimRemove(const char* _Left, char* _Right)
+{
+    int RightCnt = 0;
+    int LeftCnt = 0;
+    int count = 0;
+
+    while ( 0 != _Left[count])
+    {
+        if (_Left[LeftCnt] != ' ')
+        {
+            _Right[RightCnt] = _Left[LeftCnt];
+
+            RightCnt += 1;
+            LeftCnt += 1;
+        }
+        else
+        {
+            LeftCnt += 1;
+        }
+        _Left[count += 1];
+    }
+    return;
+}
+
+int main()
+{
+    char Text[10000] = { 0, };
+    StringTrimRemove("a b c   d   ef   ghi  j k l mno p     q r s tuv w xy z  ", Text);
+    printf_s(Text);
+}
