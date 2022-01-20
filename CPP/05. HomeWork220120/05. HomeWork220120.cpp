dbb63addd6b15a15ecccee596c758c5cfb33f0dd
woo3063 <woo3063@naver.com>
2022-01-20 16:02:14 +0900

    박종우 : 220120 메모리 구조

---
+++ CPP/05. HomeWork220120/05. HomeWork220120.cpp
@@ -0,0 +1,62 @@
+﻿// 05. HomeWork220120.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class BytePadding 
+{
+
+};
+
+class Empty
+{
+public:
+    int a;
+    int b;
+    int c;
+
+public:
+    virtual void print()
+    {
+        char arr[500] = {};
+        char* ptr = &arr[0];
+        cout <<  " 주소 : " << (__int64)reinterpret_cast<void*>(ptr) << endl;
+
+        
+
+    }
+
+};
+
+
+void Print(int _Value, Empty& _ptr, int& _ptr2)
+{
+    cout << "클래스 size : " << _Value << endl;
+    cout << "클래스 시작 주소 : " << (__int64)&_ptr << endl;
+    cout << " 멤버 변수 주소 : " << (__int64)&_ptr2 << endl;
+}
+
+int main()
+{
+    int value1 = 10;
+    int value2 = 100;
+
+    int* ptr = &value1;
+
+    int& ref = value1;    
+
+    Empty newEmpty = Empty();
+
+    int size = sizeof(newEmpty);
+
+    Print(size, newEmpty, newEmpty.a);
+
+    newEmpty.print();
+
+
+    int a = 0;
+
+}
+
---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+void Swap(int* _ptr1, int* _ptr2)
+{
+	int* temp = _ptr1;
+	_ptr1 = _ptr2;
+	_ptr2 = temp;
+
+	//*_ptr1 = 100;
+}
+
+void doubleSwap(int** _ptr1, int** _ptr2)
+{
+	int* temp = *_ptr1;
+	*_ptr1 = *_ptr2;
+	*_ptr2 = temp;
+}
+
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+
+	int* ptr1 = &num1;
+	int* ptr2 = &num2;
+	Swap(ptr1, ptr2);
+	//doubleSwap(&ptr1, &ptr2);
+
+	printf("%d, %d\n", *ptr1, *ptr2);
+
+}
